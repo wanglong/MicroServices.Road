52d034c99e50d56d2bb45a033bf222bc0fc0f1ae
lizhizhou <Administrator@DESKTOP-085QJUU>
2018-10-19 11:35:00 +0800

    add rpc demo

---
+++ LZZ.DEV.WebServer/Rpc.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rpc.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
